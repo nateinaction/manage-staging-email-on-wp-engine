49115cade1ed11d35ef627edb59a4942523bac80
Nate Gay <nate.gay@wpengine.com>
2017-11-12 19:05:05 -0600

    completed first pass on tests for Settings class

--- tests/unit/SettingsTest.php
+++ tests/unit/SettingsTest.php
@@ -5,18 +5,16 @@ namespace nategay\manage_staging_email_wpe;
 class SettingsTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Sanitize options before setting in DB
+     * Test setPluginOptions()
      *
-     * @param options_array array
-     * @param valid_email bool
+     * @param $options_array array
+     * @param $valid_email bool
      * @param $expect array
      *
      * @dataProvider dataSetPluginOptions
      */
     public function testSetPluginOptions($options_array, $valid_email, $expect)
     {
-        //$this->markTestSkipped('Skipping');
-
         $mock = $this->getMockBuilder('nategay\manage_staging_email_wpe\Settings')
             ->setMethods(array('getPluginOptions', 'checkForValidEmail', 'setOptionsInDb'))
             ->getMock();
@@ -84,6 +82,90 @@ class SettingsTest extends \PHPUnit_Framework_TestCase
                     'status' => false,
                     'message' => 'Please enter a valid email.',
                 ),
+            ),
+        );
+    }
+
+    /**
+     * Test getPluginOptions()
+     *
+     * @param $options_array bool|array False if does not exist else array
+     * @param $expect array
+     *
+     * @dataProvider dataGetPluginOptions
+     */
+    public function testGetPluginOptions($options_array, $expect)
+    {
+        $mock = $this->getMockBuilder('nategay\manage_staging_email_wpe\Settings')
+            ->setMethods(array('getOptionsFromDb'))
+            ->getMock();
+
+        $mock->method('getOptionsFromDb')->will($this->returnValue($options_array));
+
+        $this->assertEquals($mock->getPluginOptions(), $expect);
+    }
+
+    public function dataGetPluginOptions()
+    {
+        return array(
+            array(
+                false,
+                array(
+                    'email_preference' => 'admin',
+                    'custom_address' => '',
+                ),
+            ),
+            array(
+                array(
+                    'email_preference' => 'custom',
+                    'custom_address' => 'poke@poke.com',
+                ),
+                array(
+                    'email_preference' => 'custom',
+                    'custom_address' => 'poke@poke.com',
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Test getPreferredAddress()
+     *
+     * @param $options_array array
+     * @param $expect string
+     *
+     * @dataProvider dataGetPreferredAddress
+     */
+    public function testGetPreferredAddress($options_array, $admin_email, $expect)
+    {
+        $mock = $this->getMockBuilder('nategay\manage_staging_email_wpe\Settings')
+            ->setMethods(array('getPluginOptions', 'getAdminEmail'))
+            ->getMock();
+
+        $mock->method('getPluginOptions')->will($this->returnValue($options_array));
+        $mock->method('getAdminEmail')->will($this->returnValue($admin_email));
+
+        $this->assertEquals($mock->getPreferredAddress(), $expect);
+    }
+
+    public function dataGetPreferredAddress()
+    {
+        return array(
+            array(
+                array(
+                    'email_preference' => 'admin',
+                    'custom_address' => '',
+                ),
+                'admin@email.com',
+                'admin@email.com',
+            ),
+            array(
+                array(
+                    'email_preference' => 'custom',
+                    'custom_address' => 'poke@poke.com',
+                ),
+                'admin@email.com',
+                'poke@poke.com',
             ),
         );
     }
